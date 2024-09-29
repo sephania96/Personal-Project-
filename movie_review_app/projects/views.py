9f84944dc102464afeb4ff00edc215b4425f28df
Sephania96 <koutanyi@yahoo.com>
2024-09-29 09:08:52 +0100

    progress

--- movie_review_app/projects/views.py
+++ movie_review_app/projects/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .models import project
 # Create your views here.
+
+def project_index(request):
+    projects = project.objects.all()
+    context = {
+        "projects": projects
+    }
+    return render(request, "projects/project_index.html", context)
+
+def project_detail(request, pk):
+    project = project.objects.get(pk=pk)
+    context = {
+        "project": project
+    }
+    return render(request, "projects/project_detail.html", context)
