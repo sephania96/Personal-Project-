b06bba407c1bb6d573226635bd202e1e51cdc7c4
Sephania96 <koutanyi@yahoo.com>
2024-09-30 05:09:32 +0100

    imagefield to settings

--- movie_review_app/projects/admin.py
+++ movie_review_app/projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from projects.models import project
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(project, ProjectAdmin)
--- movie_review_app/projects/models.py
+++ movie_review_app/projects/models.py
@@ -6,3 +6,4 @@ class project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='project_image/', blank=True)
