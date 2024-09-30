b1a61a14280e186c0b6c631c18adc6f1c5afb9fb
Sephania96 <koutanyi@yahoo.com>
2024-09-30 05:42:37 +0100

    final on movie page

--- movie_review_app/projects/models.py
+++ movie_review_app/projects/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
+    rating = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    movie_type = models.CharField(max_length=200,default='Action')
     image = models.ImageField(upload_to='project_image/', blank=True)
